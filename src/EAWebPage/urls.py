a0c380cb28d440da25a4cbe6bb0902665bf57242
Ignacio Soto <juan.sotomontero@ucr.ac.cr>
2021-06-29 12:18:02 -0600

    Una forma que encontré de integrar el html5 player a código del Django, no lo he probado!!

--- src/EAWebPage/urls.py
+++ src/EAWebPage/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+"from eawebpage.vista import index"
 urlpatterns = [
     path('admin/', admin.site.urls),
+    "path('index/', index)"
 ]
